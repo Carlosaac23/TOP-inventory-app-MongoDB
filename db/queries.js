a14e371bef2b0ba79e14f1832f21227f7240b3cc
Carlos <carlosaac232001@gmail.com>
2026-02-12 17:32:13 -0500

    feat: create queries to get items by collection and filters, get one item by id and get all categories by type, scales and brands

--- db/queries.js
+++ db/queries.js
@@ -1,15 +1,39 @@
+import { ObjectId } from 'mongodb';
+
 import { client } from './client.js';
 
 const database = client.db('Inventory');
 
-export async function addTrain(train) {
-  await database.collection('trains').insertOne(train);
+export async function getAllItems(collection, { category, scale, brand }) {
+  const filter = {};
+
+  if (category) {
+    filter.category_id = new ObjectId(category);
+  }
+  if (scale) {
+    filter.scale_id = new ObjectId(scale);
+  }
+  if (brand) {
+    filter.brand_id = new ObjectId(brand);
+  }
+
+  return await database.collection(collection).find(filter).toArray();
 }
 
-export async function addBrand(brand) {
-  await database.collection('brands').insertOne(brand);
+export async function getItemById(collection, id) {
+  return await database
+    .collection(collection)
+    .findOne({ _id: new ObjectId(id) });
+}
+
+export async function getCategoriesByType(item) {
+  return await database.collection(`${item}_categories`).find({}).toArray();
+}
+
+export async function getAllScales() {
+  return await database.collection('scales').find({}).toArray();
 }
 
 export async function getAllBrands() {
-  return database.collection('brands').find({}).toArray();
+  return await database.collection('brands').find({}).toArray();
 }

ac1924187db991e9340d96925289ec27c139635f
Carlos <carlosaac232001@gmail.com>
2026-02-12 10:36:35 -0500

    feat: set view engine, css static file and main routes for project

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-console.log('Hello, World');
+import path from 'node:path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+import express from 'express';
+
+import connectDB from './db/client.js';
+import { generalRoute } from './routes/generalRoutes.js';
+
+connectDB();
+const app = express();
+const PORT = 8080;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const assetsPath = path.join(__dirname, 'public');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(assetsPath));
+app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.locals.currentPath = req.path;
+  next();
+});
+
+app.use('/', generalRoute);
+
+app.listen(PORT, error => {
+  if (error) throw error;
+  console.log(`Running at http://localhost:${PORT}`);
+});
